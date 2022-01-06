34d19d3fc9f8c2a7307bb39dcae1bfd12c21a435
tallesSantos <talles.saint@gmail.com>
2022-01-06 20:05:48 -0300

    Main view controller

---
+++ src/main/java/common/workshopjavafxjdbc/MainViewController.java
@@ -0,0 +1,39 @@
+package common.workshopjavafxjdbc;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemSeller;
+    @FXML
+    private MenuItem menuItemDepartment;
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction(){
+        System.out.println("onMenuItemSellerAction");
+    }
+
+    @FXML
+    public void onMenuItemDepartment(){
+        System.out.println("onMenuItemDepartment");
+    }
+
+    @FXML
+    public void onMenuItemAbout(){
+        System.out.println("onMenuItemAbout");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
