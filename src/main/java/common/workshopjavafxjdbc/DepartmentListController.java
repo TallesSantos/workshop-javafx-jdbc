35acd1e4ffd447b060540973ffb4a23696ba9aa7
tallesSantos <talles.saint@gmail.com>
2022-01-07 10:12:40 -0300

    DepartmentForm

--- src/main/java/common/workshopjavafxjdbc/DepartmentListController.java
+++ src/main/java/common/workshopjavafxjdbc/DepartmentListController.java
@@ -1,15 +1,24 @@
 package common.workshopjavafxjdbc;
 
+import common.workshopjavafxjdbc.gui.util.Alerts;
+import common.workshopjavafxjdbc.gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -52,8 +61,10 @@ public class DepartmentListController implements Initializable {
     }
 
     @FXML
-    private void onBtNewAction(){
+    private void onBtNewAction(ActionEvent event){
         System.out.println("onBtNewAction");
+    Stage parentStage = Utils.currentStage(event);
+        creatingDialogForm("DepartmentForm.fxml", parentStage);
     }
 
     public void updateTableView() throws IllegalAccessException {
@@ -64,4 +75,22 @@ public class DepartmentListController implements Initializable {
         obsList = FXCollections.observableList(list);
         tableViewDepartment.setItems(obsList);
     }
+
+    private void creatingDialogForm(String absoluteName, Stage parentStage){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            Pane pane = loader.load();
+            Stage dialogueStage = new Stage();
+            dialogueStage.setTitle("Enter Department data");
+            dialogueStage.setScene(new Scene(pane));
+            dialogueStage.setResizable(false);
+            dialogueStage.initOwner(parentStage);
+            dialogueStage.initModality(Modality.WINDOW_MODAL);
+            dialogueStage.showAndWait();
+
+        }catch (IOException e){
+        Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
 }
