cdd67236fda07816bf1d58276c0f77ea5e5e3221
tallesSantos <talles.saint@gmail.com>
2022-01-06 22:12:22 -0300

    About view

--- src/main/java/common/workshopjavafxjdbc/Main.java
+++ src/main/java/common/workshopjavafxjdbc/Main.java
@@ -2,7 +2,6 @@ package common.workshopjavafxjdbc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
@@ -10,6 +9,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private static Scene mainScene;
+
     @Override
     public void start(Stage primaryStage) {
         try {
@@ -19,13 +21,17 @@ public class Main extends Application {
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
 
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Scene getMainScene(){
+        return mainScene;
     }
 
     public static void main(String[] args) {
